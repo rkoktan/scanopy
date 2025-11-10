27fc8a2e523e206750eb754c44e0824e5d8bf9bb
vhsdream <punk.sand7393@fastmail.com>
2025-11-10 11:16:33 -0500

    fix linting

--- backend/src/server/services/definitions/proxmox_backup_server.rs
+++ backend/src/server/services/definitions/proxmox_backup_server.rs
@@ -28,7 +28,8 @@ impl ServiceDefinition for ProxmoxBackupServer {
     fn logo_url(&self) -> &'static str {
         "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/proxmox.svg"
     }
-
 }
 
-inventory::submit!(ServiceDefinitionFactory::new(create_service::<ProxmoxBackupServer>));
+inventory::submit!(ServiceDefinitionFactory::new(
+    create_service::<ProxmoxBackupServer>
+));
