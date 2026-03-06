2d5f66529886de2fc148be2f7b42de772e6f57d0
rkoktan <rkoktan@suse.com>
2026-03-06 11:41:32 -0500

    add service definition for salt project

---
+++ backend/src/server/services/definitions/saltproject.rs
@@ -0,0 +1,34 @@
+use crate::server::ports::r#impl::base::PortType;
+use crate::server::services::definitions::{ServiceDefinitionFactory, create_service};
+use crate::server::services::r#impl::categories::ServiceCategory;
+use crate::server::services::r#impl::definitions::ServiceDefinition;
+use crate::server::services::r#impl::patterns::Pattern;
+
+#[derive(Default, Clone, Eq, PartialEq, Hash)]
+pub struct Salt-Project;
+
+impl ServiceDefinition for Salt Project {
+    fn name(&self) -> &'static str {
+        "Salt Project"
+    }
+    fn description(&self) -> &'static str {
+        "Salt Project - Salt Masters & Salt Minions"
+    }
+    fn category(&self) -> ServiceCategory {
+        ServiceCategory::NetworkCore
+    }
+
+    fn discovery_pattern(&self) -> Pattern<'_> {
+        Pattern::Endpoint(
+            PortType::new_tcp(4505),
+            PortType::new_tcp(4506),
+            None,
+        )
+    }
+
+    fn logo_url(&self) -> &'static str {
+        "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/salt-project.svg"
+    }
+}
+
+inventory::submit!(ServiceDefinitionFactory::new(create_service::<Salt Project>));
